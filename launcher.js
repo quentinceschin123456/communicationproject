53b88b5307706888d8a65e3f2a6f025b8f734086
Quentin Ceschin <ceschin.quentin@gmail.com>
2020-02-15 23:21:26 +0100

    update launcher

--- launcher.js
+++ launcher.js
@@ -5,5 +5,6 @@ const PORT = process.env.PORT || 8000
 express()
   .use(express.static(path.join(__dirname, 'ressources')))
   .set('views', path.join(__dirname, 'src/views'))
+  .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
