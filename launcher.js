a79d2476cf3df01ece1cde82775513e4bd5ae4b0
Quentin Ceschin <ceschin.quentin@gmail.com>
2020-02-15 19:17:12 +0100

    start node app

--- launcher.js
+++ launcher.js
@@ -1,12 +1,10 @@
-const express = require("express");
-const port = process.env.PORT || 8080;
-const app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + "/dist/index.html");
-});
-app.listen(port);
-
-// eslint-disable-next-line no-console
-console.log("Server started...");
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
